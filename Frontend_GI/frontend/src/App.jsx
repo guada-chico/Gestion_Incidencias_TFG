e3a36e1bc7bcc014c9e4168f0f7dcd19463a042d
Guada <guadalupe.fernandez@des.kyocera.com>
2026-04-10 14:26:03 +0200

    menú hamburguesa arreglado (falta fijar)

--- Frontend_GI/frontend/src/App.jsx
+++ Frontend_GI/frontend/src/App.jsx
@@ -1,29 +1,49 @@
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
+import { useState } from 'react'
+
 import IncidentList from './pages/IncidentList'
 import IncidentDetail from './pages/IncidentDetail'
 import IncidentForm from './pages/IncidentForm'
 import Login from './components/Login'
 import NavBar from './components/NavBar'
+
 import kyoImg from './assets/Kyocera_logo.svg.png'
+import kyoImgMini from './assets/kyocera.png'
 import './App.css'
 
 /* =========================
-   LAYOUT PRINCIPAL (SIDEBAR + HEADER)
+   LAYOUT PRINCIPAL
 ========================= */
 function AppLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+
   return (
     <div className="app-layout">
 
-      {/* SIDEBAR */}
-      <aside className="sidebar">
-        <NavBar />
-      </aside>
+      {/* SIDEBAR (NORMAL / MINI) */}
+      {sidebarOpen ? (
+        <aside className="sidebar">
+          <NavBar />
+        </aside>
+      ) : (
+        <aside className="sidebar-mini">
+        </aside>
+      )}
 
       {/* CONTENIDO PRINCIPAL */}
       <div className="main-area">
 
         {/* HEADER */}
         <header className="hero">
+
+          {/* BOTÓN TOGGLE */}
+          <button
+            className="toggle-btn"
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+          >
+            ☰
+          </button>
+
           <img src={kyoImg} className="base" alt="Kyocera Logo" />
           <h1>Gestión de Incidencias</h1>
         </header>
@@ -34,8 +54,8 @@ function AppLayout() {
             <Outlet />
           </div>
         </main>
-      </div>
 
+      </div>
     </div>
   )
 }
@@ -51,7 +71,7 @@ function App() {
         {/* LOGIN SIN SIDEBAR */}
         <Route path="/login" element={<Login />} />
 
-        {/* APP CON SIDEBAR + HEADER */}
+        {/* APP PRINCIPAL */}
         <Route element={<AppLayout />}>
           <Route path="/" element={<IncidentList />} />
           <Route path="/crear" element={<IncidentForm />} />
