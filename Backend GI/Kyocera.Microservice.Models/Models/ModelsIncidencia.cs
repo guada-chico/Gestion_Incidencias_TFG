c8f28206304ae15f0c867546ec9942e35533dc47
JuliaMuelas <julia.muelas@des.kyocera.com>
2026-04-16 09:47:58 +0200

    PRIORIDAD CAMBIADA

--- Backend GI/Kyocera.Microservice.Models/Models/ModelsIncidencia.cs
+++ Backend GI/Kyocera.Microservice.Models/Models/ModelsIncidencia.cs
@@ -7,7 +7,7 @@ namespace Kyocera.Microservice.Models.Models
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Estado Estado { get; set; }
-        public Estado Prioridad { get; set; }
+        public Prioridad Prioridad { get; set; }
         public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
         public DateTime? FechaLimite { get; set; }
     }
