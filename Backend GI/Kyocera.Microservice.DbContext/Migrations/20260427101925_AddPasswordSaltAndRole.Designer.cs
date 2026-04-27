3826f82cee1951c23ba24a21255e69b438435524
Guada <guadalupe.fernandez@des.kyocera.com>
2026-04-27 12:28:12 +0200

    migración nueva tabla usuario

--- Backend GI/Kyocera.Microservice.DbContext/Migrations/20260427101925_AddPasswordSaltAndRole.Designer.cs
+++ Backend GI/Kyocera.Microservice.DbContext/Migrations/20260427101925_AddPasswordSaltAndRole.Designer.cs
@@ -4,6 +4,7 @@ using Kyocera.Microservice.DbContext.BoundedContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kyocera.Microservice.DbContext.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260427101925_AddPasswordSaltAndRole")]
+    partial class AddPasswordSaltAndRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend GI/Kyocera.Microservice.DbContext/Migrations/20260427101925_AddPasswordSaltAndRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kyocera.Microservice.DbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordSaltAndRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Usuarios");
+        }
+    }
+}
